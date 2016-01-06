71e91c4019ba8a5db568996cdd82a1c662f10537
CDMCMBP-004 <CDMC_009@CDMCMBP-004.local>
2016-01-06 16:59:11 +0800

    fist time functional

--- gmail_api_try/gmail_try.py
+++ gmail_api_try/gmail_try.py
@@ -27,11 +27,18 @@ http = httplib2.Http()
 http = credentials.authorize(http)
 service = discovery.build('gmail', 'v1', http=http)
 
+def query_mail(gmail_service, user, query_mail):
+	try:
+		query_response = gmail_service.users().messages().list(userId = user, q = query_mail).execute()
+		return query_response
+	except errors.HttpError, error:
+		print ('An error occures : ',error)
+
 try:
 	user_profile_response = service.users().getProfile(userId='me').execute()
 	print ('messagesTotal:'+str(user_profile_response["messagesTotal"]))
 
-	user_mail_response = service.users().messages().get(userId = 'me', id ='151e7cc2cce9a31a', format='full').execute()
+	"""user_mail_response = service.users().messages().get(userId = 'me', id ='151e7cc2cce9a31a', format='full').execute()
 	for part in user_mail_response['payload']['parts']:
 		
 		if part['filename']:
@@ -41,12 +48,10 @@ try:
 			f = open(part['filename'],'w')
 			f.write(base64.urlsafe_b64decode(data['data'].encode("utf-8")))
 			f.close()
+	"""
 
-	query = "subject:研究所指導老師相關事宜詢問"
-	user_mail_query_subject_response = service.users().messages().list(userId = 'me', q = query).execute()
-	#print (json.dumps(user_mail_query_subject_response))
-	for mail_ids in user_mail_query_subject_response['messages']:
-		print ('mail id:'+str(mail_ids['id']))
-	
+	query_result = query_mail(service, 'me', "subject:研究所指導老師相關事宜詢問")
+	for mail_id in query_result['messages']:
+		print('mail id : '+str(mail_id['id']))
 except errors.HttpError, error:
 	print('An error occured :',error)
