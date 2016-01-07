461e141373d5441354f819385c699ebfe627f053
CDMCMBP-004 <CDMC_009@CDMCMBP-004.local>
2016-01-07 17:06:49 +0800

    update

--- gmail_connecter.py
+++ gmail_connecter.py
@@ -2,7 +2,7 @@
 # -*- coding: utf8 -*-
 
 from oauth2client import file, client, tools
-import httplib2, base64, sys, json, os, mimetypes
+import httplib2, base64, sys, json, os, mimetypes, argparse
 from apiclient import discovery, errors
 from email.mime.audio import MIMEAudio
 from email.mime.base import MIMEBase
@@ -19,10 +19,19 @@ def certificate():
 
 	if not credentials or credentials.invalid:
 		flow = client.flow_from_clientsecrets(CLIENT_SECRET,scope=SCOPES,redirect_uri='http://localhost')
+		flow.user_agent = flow.step1_get_authorize_url()
+		parser = argparse.ArgumentParser(parents=[tools.argparser])
+		flags = parser.parse_args()
+		credentials = tools.run_flow(flow, storage, flags)
+		"""
+		#by using browser
+		flow = client.flow_from_clientsecrets(CLIENT_SECRET,scope=SCOPES,redirect_uri='http://localhost')
 		auth_uri = flow.step1_get_authorize_url()
+		print('請連接下列網址進行認證，並將回傳的code輸入程式中')
 		print(auth_uri)
 		auth_code=raw_input("-->")
 		credentials = flow.step2_exchange(auth_code)
+		"""
 		storage.put(credentials)
 
 	http = httplib2.Http()
@@ -30,24 +39,40 @@ def certificate():
 	service = discovery.build('gmail', 'v1', http=http)
 	return service
 
-def query_mail(gmail_service, user, query_mail):
+def list_mail(gmail_service, user, max_result ):
 	try:
-		query_response = gmail_service.users().messages().list(userId = user, q = query_mail).execute()
+		query_response = gmail_service.users().messages().list(userId = user, maxResults = max_result).execute()
 		return query_response
 	except (errors.HttpError, error):
 		#print ('An error occures : ',error)
 		return error
 
+def query_mail(gmail_service, user, query_mail):
+	try:
+		query_response = gmail_service.users().messages().list(userId = user, q = query_mail, maxResults = 50).execute()
+		return query_response
+	except (errors.HttpError, error):
+		#print ('An error occures : ',error)
+		return error
+
+def get_mail_detail(gmail_service, user, mailId):
+	try:
+		mail_detail = gmail_service.users().messages().get(userId = user, id = mailId , format = 'full').execute()
+		return mail_detail
+	except:
+		pass
+
 def download_files(gmail_service, user, mailId):
 	try:
-		get_attachmentId = gmail_service.users().messages().get(userId = user, id = mailId , format = 'full').execute()
+		get_attachmentId = get_mail_detail(gmail_service, user, mailId)
 		for parts in get_attachmentId['payload']['parts']:
 			if parts['filename']:
 				data = gmail_service.users().messages().attachments().get(userId = user, messageId = mailId, id = parts['body']['attachmentId']).execute()
 				output = open(parts['filename'],'w')
 				output.write(base64.urlsafe_b64decode(data['data'].encode("utf-8")))
 				output.close
-				#print (parts['filename'].encode("utf-8")," download done!")
+				print (str(parts['filename'].encode("utf-8"))+" download done!")
+				#print ('download done !')
 			else :
 				#print ("don't have attachment !")
 				pass
@@ -59,29 +84,34 @@ def download_files(gmail_service, user, mailId):
 def send_message(gmail_service, user, message):
 	try:
 		send_mail = gmail_service.users().messages().send(userId = user, body = message).execute()
-		print ("Message Id : ",send_mail['id'])
+		#print ("Message Id : ",send_mail['id'])
+		print ('mail sended')
 		return send_mail
 	except:
 		pass
 
 def create_message(sender, to, subject, message_text):
 	message = MIMEText(message_text)
-	message['to'] = to
-	message['from'] = sender
-	message['subject'] = subject
+	message['To'] = to
+	message['From'] = sender
+	message['Subject'] = subject
 	return {'raw': base64.b64encode(message.as_string())}
 
 def create_message_with_attachment(sender, to, subject, message_text, file_path):
 	message = MIMEMultipart()
-	message['to'] = to
-	message['from'] = sender
-	message['subject'] = subject
+	message['To'] = to
+	message['From'] = sender
+	message['Subject'] = subject
 
 	msg = MIMEText(message_text)
 	message.attach(msg)
 
 	path = os.path.expanduser(file_path)
 	filename = path.split('/')[-1]
+
+	#print (path)
+	#print (filename)
+	#print os.path.exists(path)
 	content_type, encoding = mimetypes.guess_type(path)
 
 	if content_type is None or encoding is not None:
@@ -117,6 +147,7 @@ if __name__ == '__main__':
 		user_profile_response = service.users().getProfile(userId='me').execute()
 		print ('messagesTotal:'+str(user_profile_response["messagesTotal"]))
 
+		"""
 		mail_from = raw_input("mail from :")
 		mail_to = raw_input("mail to :")
 		mail_subject = raw_input("subject :")
@@ -125,12 +156,10 @@ if __name__ == '__main__':
 
 		mail = create_message_with_attachment(mail_from,mail_to,mail_subject,mail_message,path)
 		send_message(service,'me',mail)
-		"""
-		mail = create_message_with_attachment("purpledoor4921@gmail.com","purpledoor4921@gmail.com","test case","test","","cover.jpg")
+		
+		mail = create_message("purpledoor4921@gmail.com","purpledoor4921@gmail.com","test case","test")
 		send_message(service,'me',mail)
-		"""
-
-		"""
+		
 		query_subject = raw_input("subject : ")
 		query_result = query_mail(service, 'me', "subject:"+query_subject)
 		for mail_id in query_result['messages']:
